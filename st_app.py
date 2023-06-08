8ec3ac288e663acff4dfd8265a23c59331e01d55
gVICKOS <90777936+gVICKOS@users.noreply.github.com>
2023-06-08 21:31:45 +0200

    Update st_app.py

--- st_app.py
+++ st_app.py
@@ -2,6 +2,6 @@ import streamlit as st
 #import math
 import pickle
 st.write('hello world') 
-file_pi2= open('piii','rb')
+file_pi2= open('piii.pkl','rb')
 obj= pickle.load(file_pi2)
 st.write('obj')
